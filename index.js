8c23aa7c15ac7b1b25e8912a090dafc440bf54d3
Eric-Catarina <2020954200@teiacoltec.org>
2022-08-26 20:00:16 -0300

    Tests

--- index.js
+++ index.js
@@ -4,46 +4,60 @@ let fibonumbers = []
 let productsArray = []
 let factorsArray = []
 let fibonumber
+let factorsArrayWithProd = productsArray
 
-for (let i = 0; i < 150; i++) {
-  fibonumber = next + current
-  current = next
-  next = fibonumber
-  fibonumbers.push(fibonumber)
 
-  productsArray.push(fibonumber * current)
-  factorsArray.push(fibonumber, current)
+for (let i = 0; i < 30; i++) {
+    fibonumber = next + current
+    current = next
+    next = fibonumber
+    fibonumbers.push(fibonumber)
+
+    productsArray.push(fibonumber * current)
+    factorsArray.push(fibonumber, current)
 }
 
 function ReturnTheTwoFactorsOfFalseNumber(number) {
-  function CompareNumbers(a, b) {
-    return a - b
-  }
-  
-  let factorsArrayWithProd = productsArray
-  factorsArrayWithProd.push(number)
-  factorsArrayWithProd.sort(CompareNumbers)
-  let indexOfTheProduct = factorsArrayWithProd.indexOf(number)
-  let firstFactor = (factorsArray[2*indexOfTheProduct])
-  let secondFactor = (factorsArray[(2*indexOfTheProduct) + 1]) 
 
-  return [secondFactor, firstFactor, false]
+    function CompareNumbers(a, b) {
+        return a - b
+    }
+
+    factorsArrayWithProd = productsArray
+    factorsArrayWithProd.push(number)
+    factorsArrayWithProd.sort(CompareNumbers)
+
+    
+    let indexOfTheProduct = factorsArrayWithProd.indexOf(number)
+    let firstFactor_ = (factorsArray[2 * indexOfTheProduct] - 1)
+    let secondFactor_ = (factorsArray[(2 * indexOfTheProduct) ])
+
+    return [secondFactor_, firstFactor_, false]
 }
 
-function ReturnTwoFactorsOfTrueNumber(number){
-let indexOfTheProduct = productsArray.indexOf(number)
-  
-let firstFactor = factorsArray[(indexOfTheProduct*2)]
-let secondFactor = factorsArray[(indexOfTheProduct*2) + 1]
+function ReturnTwoFactorsOfTrueNumber(number) {
+    let indexOfTheProduct = productsArray.indexOf(number)
 
-return [secondFactor, firstFactor, true]
+    let firstFactor = factorsArray[(indexOfTheProduct * 2)]
+    let secondFactor = factorsArray[(indexOfTheProduct * 2) + 1]
+
+    return [secondFactor, firstFactor, true]
 }
+
 function productFib(prod) {
-  if (productsArray.includes(prod)){
-    return ReturnTwoFactorsOfTrueNumber(prod)
-  }
-  else{
-    return ReturnTheTwoFactorsOfFalseNumber(prod)
-  }
+    if (productsArray.includes(prod)) {
+        return ReturnTwoFactorsOfTrueNumber(prod)
+    }
+    else {
+        return ReturnTheTwoFactorsOfFalseNumber(prod)
+    }
 }
 
+console.log(productFib(4895))
+console.log(productFib(5895))
+console.log(productFib(74049690))
+console.log(productFib(84049690))
+console.log(productFib(193864606))
+console.log(productFib(447577))
+console.log(productFib(602070))
+  
